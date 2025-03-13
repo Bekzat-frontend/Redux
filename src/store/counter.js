9bab4c54dc9f3fa41db5649f2a24178b54e4ed20
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-13 21:37:24 +0700

    remove

---
+++ src/store/counter.js
@@ -0,0 +1,20 @@
+// import { createStore } from "redux";
+
+// const initial = {
+//   counter: 0,
+// };
+
+// const counterReducer = (state = initial, action) => {
+//   switch (action.type) {
+//     case "increament":
+//       return { counter: state.counter + 1 };
+//     case "decrement":
+//       return state.counter > 0 ? { counter: state.counter - 1 } : state;
+//     default:
+//       return state;
+//   }
+// };
+
+// const store = createStore(counterReducer);
+
+// export default store;
