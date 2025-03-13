2718a8e39fe1798b96032353d46d78e367091a6b
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-13 20:21:17 +0700

    todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+// import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
 
-function App() {
-  const store = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
-  const increament = () => {
-    dispatch({ type: "increament" });
-  };
-  const decrement = () => {
-    dispatch({ type: "decrement" });
-  };
-  return (
-    <div>
-      <button onClick={increament}>+</button>
-      <h1>San:{store}</h1>
-      <button onClick={decrement}>-</button>
-    </div>
-  );
-}
+// function App() {
+//   const store = useSelector((state) => state.counter);
+//   const dispatch = useDispatch();
+//   const increament = () => {
+//     dispatch({ type: "increament" });
+//   };
+//   const decrement = () => {
+//     dispatch({ type: "decrement" });
+//   };
+//   return (
+//     <div>
+//       <button onClick={increament}>+</button>
+//       <h1>San:{store}</h1>
+//       <button onClick={decrement}>-</button>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+  
