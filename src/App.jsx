c5bd65fafe386dda9d7e525edc672bd1a7f712a4
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-13 20:03:23 +0700

    dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const store = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const increament = () => {
+    dispatch({ type: "increament" });
+  };
+  const decrement = () => {
+    dispatch({ type: "decrement" });
+  };
   return (
     <div>
-      
+      <button >+</button>
+      <h1>{store}</h1>
+      <button>-</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
